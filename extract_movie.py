e5fb8417f468d1e0fedc9c5ed3b91247ec05c4ac
AlbertoD94 <adleme94@gmail.com>
2020-12-10 19:33:12 -0500

    more movies

--- extract_movie.py
+++ extract_movie.py
@@ -5,7 +5,7 @@ import requests
 LOCAL_BASE_PATH = os.path.abspath(os.getcwd())
 LINKS_PATH = os.path.abspath(os.path.join(LOCAL_BASE_PATH, 'dataset', 'links.csv'))
 
-API_KEY = '1fcde7b2'
+API_KEY = '1ad91603'
 URL = 'http://www.omdbapi.com/'
 
 PARAMS = {
